fca8d12c2c03466d66f83a9465c96680c643cf3b
Lucas-contrib <139517304+Lucas-contrib@users.noreply.github.com>
2026-04-30 11:19:34 -0300

    Create setup script for Neovim and AutoHotkey
    This script sets up Neovim and AutoHotkey on the user's desktop, including creating necessary directories and configuration files.

---
+++ script.bat
@@ -0,0 +1,19 @@
+mkdir "C:\Users\Navegador\Desktop\temp"
+cd "C:\Users\Navegador\Desktop\temp"
+curl -LO "https://github.com/neovim/neovim/releases/download/v0.12.2/nvim-win64.zip"
+tar -xf nvim-win64.zip
+mkdir "C:\Users\Navegador\AppData\Local\nvim"
+echo :colorscheme industry > "C:\Users\Navegador\AppData\Local\nvim\init.vim"
+echo :set tabstop=2 >> "C:\Users\Navegador\AppData\Local\nvim\init.vim"
+echo :set shiftwidth=2 >> "C:\Users\Navegador\AppData\Local\nvim\init.vim"
+echo :set nowrap >> "C:\Users\Navegador\AppData\Local\nvim\init.vim"
+pause
+curl -LO "https://github.com/AutoHotkey/AutoHotkey/releases/download/v2.0.24/AutoHotkey_2.0.24_setup.exe"
+mkdir "C:\Users\Navegador\Desktop\temp\ahk"
+echo "Instalando autohotkey en temp"
+AutoHotkey_2.0.24_setup.exe /silent /to "C:\Users\Navegador\Desktop\temp\ahk"
+capslock-rebind.ahk
+move capslock-rebind.ahk "C:\Users\Navegador\Desktop\temp"
+C:\Users\Navegador\Desktop\temp\capslock-rebind.ahk
+cd ..
+rmdir /s /q nvim-win-script
